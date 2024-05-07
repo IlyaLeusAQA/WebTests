3e18247448dcb18995b8d9b6de205f1ee502d84b
Ilya Leus <ilyaleus@gmail.com>
2024-05-07 21:58:53 +0700

    add new file LoginTests and refactor project files

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -7,7 +7,8 @@ class BasePage:
         self.driver = driver
 
     def find_element(self, locator, time=5):
-        return WebDriverWait(self.driver, time).until(expected_conditions.visibility_of_element_located(locator), message=f'не удалось найти элемент {locator}')
+        return WebDriverWait(self.driver, time).until(expected_conditions.visibility_of_element_located(locator),
+                                                      message=f'не удалось найти элемент {locator}')
 
     def get_url(self, url):
         return self.driver.get(url)
